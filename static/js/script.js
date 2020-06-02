7b87936f6a38e15ec5b08cc09363e94107d3af2f
ale <aaguilera14@alumnos.uaq.mx>
2020-06-01 20:50:21 -0500

    fondo animado con degradados QUE EMOCION!!

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,8 @@ const print = document.querySelector(".print");
 const sombras = document.querySelector(".sombras");
 print.addEventListener('click',Rprint);
 sombras.addEventListener('click',Rsombras);
+constante = 15;
+
 
 function Rprint(){
     window.location = "./print";
@@ -19,7 +21,7 @@ window.onload = function(){
     var texto1 = 'Microsof Windows [Versión 10.0.18363.836]$'+                   
     '(c) 2019 Microsoft Corporation. Todos los derechos reservados.$'+
     '$'+                                                                 
-    'C:/Users/ale> Click an option';
+    'C:/Users/ale> Desliza tu dedo/mouse o da click en una opcion';
     texto2 = "pagina del print";
     texto3 = "Pagina de sombras";
     insertar(1, texto1);
@@ -44,4 +46,34 @@ function insertar(hijo, cadena){
     }
 }   
 
+function isMobile(){
+    return (
+        (navigator.userAgent.match(/Android/i)) ||
+        (navigator.userAgent.match(/webOS/i)) ||
+        (navigator.userAgent.match(/iPhone/i)) ||
+        (navigator.userAgent.match(/iPod/i)) ||
+        (navigator.userAgent.match(/iPad/i)) ||
+        (navigator.userAgent.match(/BlackBerry/i))
+    );
+}
+if(isMobile()){
+    elemento(".consola").addEventListener("touchmove", handleMove, false);
+}
+else{
+    // elemento(".consola").addEventListener('mouseover',function(){
+    //     elemento("#siguerratones").style.display = "block";
+    // });
+    elemento(".consola").addEventListener('mousemove',seguir);
+}
 
+function seguir() {
+    cuadro(parseInt(window.event.pageX), parseInt(window.event.pageY));
+}
+function handleMove(evt) {
+    cuadro(evt["changedTouches"][0]["pageX"],evt["changedTouches"][0]["pageY"]);
+    console.log(evt["changedTouches"][0]["pageX"]+"--"+evt["changedTouches"][0]["pageY"]);
+}
+function cuadro(x,y){
+    radial = "radial-gradient(circle at "+x+"px "+y+"px, transparent, black 10%)";
+    elemento(".consola").style.background = radial;
+}
