1a48fe1f3f9cfa277fd851200a3a98810a4052d9
ale <aaguilera14@alumnos.uaq.mx>
2020-06-03 21:46:39 -0500

    Se corrio error de libreira

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from requests_html import HTMLSession
 import time
 
 
